295b7100a5d4d6a5af7cce7b80f117dc528b4ddb
Andrew Lee <andrewzhlee@gmail.com>
2022-06-04 16:14:26 +0800

    fix eslint config again

--- client/.eslintrc.js
+++ client/.eslintrc.js
@@ -1,40 +1,49 @@
-// Example eslint config
-// https://medium.com/swlh/a-bit-on-eslint-configuration-in-a-react-project-c9b0eef390c4
-
-// {
-//     "extends": ["eslint:recommended", "plugin:react/recommended"],
-//     "rules": {
-//         // suppress errors for missing 'import React' in files
-//        "react/react-in-jsx-scope": "off"
-//     }
-// };
-
 module.exports = {
-    parser: "babel-eslint",
+    extends: ["eslint:recommended", "plugin:react/recommended"],
+    parserOptions: {
+        ecmaVersion: 2020, // Needed to use new js features.
+        sourceType: "module",
+    },
     env: {
         browser: true,
         node: true,
         es6: true,
         jest: true,
     },
-    extends: [
-        "eslint:recommended",
-        "plugin:react/recommended",
-        //   "plugin:jsx-a11y/recommended"
-    ],
-    plugins: [
-        "react",
-        "react-hooks",
-        //   "jsx-a11y",
-    ],
     rules: {
-        strict: 0,
-        "react-hooks/rules-of-hooks": "error",
-        "react-hooks/exhaustive-deps": "warn",
-    },
-    settings: {
-        react: {
-            version: "detect",
-        },
+        // suppress errors for missing 'import React' in files
+        "react/react-in-jsx-scope": "off",
     },
 };
+
+// Example eslint config
+// https://medium.com/swlh/a-bit-on-eslint-configuration-in-a-react-project-c9b0eef390c4
+// module.exports = {
+//     parser: "babel-eslint",
+//     env: {
+//         browser: true,
+//         node: true,
+//         es6: true,
+//         jest: true,
+//     },
+//     extends: [
+//         "eslint:recommended",
+//         "plugin:react/recommended",
+//         //   "plugin:jsx-a11y/recommended"
+//     ],
+//     plugins: [
+//         "react",
+//         "react-hooks",
+//         //   "jsx-a11y",
+//     ],
+//     rules: {
+//         strict: 0,
+//         "react-hooks/rules-of-hooks": "error",
+//         "react-hooks/exhaustive-deps": "warn",
+//     },
+//     settings: {
+//         react: {
+//             version: "detect",
+//         },
+//     },
+// };
